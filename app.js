2d9610d77b0f34b300818f080f9b70205928561a
EL AINE El Mehdi <ainemehdi6@gmail.com>
2023-04-30 19:17:32 +0200

    add cors to app.js

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
 var mongoose = require('mongoose');
+var cors = require('cors');
 
 var indexRouter = require('./routes/index');
 var exerciceRouter = require('./routes/exercice');
@@ -24,12 +24,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
-app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*"); // Allow requests from localhost:57187
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-});
+app.use(cors());
 
 app.use('/', indexRouter);
 app.use('/exercices', exerciceRouter);
